63d2af8b64eb6ad1b172075474c7ddcc8e8ba680
Tiiwoo <tiiwoo.taowei@outlook.com>
2022-05-04 12:45:45 +0800

    udpate

--- js/networkCheck.js
+++ js/networkCheck.js
@@ -68,8 +68,8 @@
      $done({
        title: wifi.ssid ? wifi.ssid : cellularInfo,
        content:
-         // (v4.primaryAddress ? `IPv4 : ${v4.primaryAddress} \n` : '') +
-         // (v6.primaryAddress ? `IPv6 : ${v6.primaryAddress}\n` : '') +
+         (v4.primaryAddress ? `IPv4 : ${v4.primaryAddress} \n` : '') +
+         (v6.primaryAddress ? `IPv6 : ${v6.primaryAddress}\n` : '') +
          // (v4.primaryRouter && wifi.ssid ? `Router IPv4 : ${v4.primaryRouter}\n` : '') +
          // (v6.primaryRouter && wifi.ssid ? `Router IPv6 : ${v6.primaryRouter}\n` : '') +
          `IP : ${info.query}\n` +
