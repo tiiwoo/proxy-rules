a2caf25ad7c0a8c68df45e674caa9306e02a0981
Tiiwoo <tiiwoo.taowei@outlook.com>
2022-10-13 11:43:33 +0800

    update revenuecat

--- Script/revenuecat.js
+++ Script/revenuecat.js
@@ -1,6 +1,9 @@
 re('"expires_date":"\\w{4}@"period_type":"\\w+"','"expires_date":"2099@"period_type":"active"');
 function re() {
   var body = $response.body;
+  if(!body) {
+    $done({});
+  }
   if (arguments[0].includes("@")) {
     var regs = arguments[0].split("@");
     var strs = arguments[1].split("@");
