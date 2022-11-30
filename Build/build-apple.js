58d532a60491e523cb7e855aaebcc346ca8f711b
Tiiwoo <tiiwoo.taowei@outlook.com>
2022-11-30 23:10:14 +0800

    add apple tpl rule

---
+++ Build/build-apple.js
@@ -0,0 +1,46 @@
+const { fetchWithRetry } = require("./lib/fetch-retry");
+const fs = require("fs");
+const path = require("path");
+
+const { isDomainLoose } = require("./lib/is-domain-loose");
+
+(async () => {
+  console.time("Total Time - build-apple-conf");
+
+  const res = (
+    await (
+      await fetchWithRetry(
+        "https://raw.githubusercontent.com/geekdada/surge-list/master/surgio-snippet/apple.tpl"
+      )
+    ).text()
+  )
+    .split("\n")
+    .map((line) => {
+      if (line.startsWith("{%")) {
+        return null;
+      } else if (line.endsWith("{{ default_rule }}")) {
+        return line.replace("{{ default_rule }}", "🍎 Apple");
+      } else if (line.endsWith("{{ api_rule }}")) {
+        return line.replace("{{ api_rule }}", "🍎 Apple");
+      } else if (line.endsWith("{{ cdn_rule }}")) {
+        return line.replace("{{ cdn_rule }}", "🍎 Apple CDN");
+      } else if (line.endsWith("{{ location_rule }}")) {
+        return line.replace("{{ location_rule }}", "DIRECT");
+      } else if (line.endsWith("{{ apple_news_rule }}")) {
+        return line.replace("{{ apple_news_rule }}", "🍎 Apple");
+      }
+
+      return null;
+    });
+  // .filter((domain) => typeof domain === "string" && isDomainLoose(domain));
+
+  await Promise.all([
+    fs.promises.writeFile(
+      path.resolve(__dirname, "../List/ruleset/apple.conf"),
+      res.map((domain) => `${domain}`).join("\n") + "\n",
+      "utf-8"
+    ),
+  ]);
+
+  console.timeEnd("Total Time - build-apple-conf");
+})();
