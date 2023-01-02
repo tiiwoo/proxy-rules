4d9bb1af861b954db396af7212fa987d024a4063
Tiiwoo <tiiwoo.taowei@outlook.com>
2023-01-02 13:07:40 +0000

    deploy: 163be66b0bb54b25d8b14839d5b0c06e1b79f5d0

--- Script/revenuecat.js
+++ Script/revenuecat.js
@@ -1,20 +1,41 @@
-re('"expires_date":"\\w{4}@"period_type":"\\w+"','"expires_date":"2099@"period_type":"active"');
-function re() {
-  var body = $response.body;
-  if(!body) {
-    $done({});
-  }
-  if (arguments[0].includes("@")) {
-    var regs = arguments[0].split("@");
-    var strs = arguments[1].split("@");
-    for (i = 0;i < regs.length;i++) {
-     var reg = new RegExp(regs[i],"g");
-     body = body.replace(reg, strs[i]);
+const resp = {};
+const obj = JSON.parse(
+  (typeof $response != 'undefined' && $response.body) || null
+);
+
+const ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
+const list = {
+  VSCO: { name: 'membership', id: 'com.circles.fin.premium.yearly' },
+  '1Blocker': { name: 'premium', id: 'blocker.ios.subscription.yearly' },
+  Anybox: { name: 'pro', id: 'cc.anybox.Anybox.annual' },
+  Fileball: { name: 'filebox_pro', id: 'com.premium.yearly' },
+  ipTV: { name: 'ipTV +', id: 'iptv_9.99_1y_7d_free' },
+};
+const data = {
+  expires_date: '2099-02-18T07:52:54Z',
+  original_purchase_date: '2020-02-11T07:52:55Z',
+  purchase_date: '2020-02-11T07:52:54Z',
+};
+
+if (typeof $response == 'undefined') {
+  delete $request.headers['x-revenuecat-etag']; // prevent 304 issues
+  delete $request.headers['X-RevenueCat-ETag'];
+  resp.headers = $request.headers;
+} else if (obj && obj.subscriber) {
+  obj.subscriber.subscriptions = obj.subscriber.subscriptions || {};
+  obj.subscriber.entitlement = obj.subscriber.entitlement || {};
+  for (const i in list) {
+    if (new RegExp(`^${i}`, `i`).test(ua)) {
+      obj.subscriber.subscriptions[list[i].id] = data;
+      obj.subscriber.entitlements[list[i].name] = JSON.parse(
+        JSON.stringify(data)
+      );
+      obj.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+      break;
     }
   }
-  else {
-    var reg = new RegExp(arguments[0],"g");
-    body = body.replace(reg, arguments[1]);
-  }
-  $done({body});
+  resp.body = JSON.stringify(obj)
+    .replace(/\"expires_date\":\"\w{4}/g, '"expires_date":"2099')
+    .replace(/\"period_type\":\"\w+\"/g, '"period_type":"active"');
 }
+$done(resp);
