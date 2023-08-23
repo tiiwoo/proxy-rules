37cbe6d0c6e01cc424e62b49a26c252954bb7339
Tiiwoo <tiiwoo.taowei@outlook.com>
2023-08-23 12:28:27 +0800

    update fantastical.js

--- Script/fantastical.js
+++ Script/fantastical.js
@@ -1,22 +1,43 @@
 let url = $request.url;
-const path1 = 'v1/auth/device';
-const path2 = 'v1/account/details';
 let obj = JSON.parse($response.body);
-if (url.indexOf(path1) != -1) {
-  obj['subscription'] = {
+
+if (url.indexOf('device') != -1) {
+  obj.status = 'success';
+  obj.subscription = {
     autorenew: true,
-    expiration: '2099-02-15T09:09:14Z',
-    expires: '2099-02-15T09:09:14Z',
+    expiration: '2099-12-31T16:49:37Z',
+    expires: '2099-12-31T16:49:37Z',
   };
+  obj.scope = [
+    'notify',
+    'weather',
+    'keyvalue-watch',
+    'keyvalue-verification',
+    'schedjoules',
+    'scheduling',
+    'account',
+    'keyvalue',
+    'fantastical',
+  ];
 }
-if (url.indexOf(path2) != -1) {
-  obj['subscription'] = {
+
+if (url.indexOf('details') != -1) {
+  obj.subscription = {
     autorenew: true,
-    expiration: '2099-02-15T09:09:14.000000Z',
-    uuid: '3c5927a6-4e3e-4773-b7af-62506e036dc7',
+    expiration: '2099-12-31T16:49:37.000000Z',
+    uuid: 'f1da7c78-e964-4367-915c-886edc794959',
     subscription_type: 'AppStore',
     is_expired: false,
     trial: false,
   };
 }
+
+if (url.indexOf('appstore-receipt') != -1) {
+  obj.autorenew = true;
+  obj.expiration = '2099-12-31T16:49:37.000000Z';
+  obj.subscription_type = 'AppStore';
+  obj.is_expired = false;
+  obj.trial = false;
+}
+
 $done({ body: JSON.stringify(obj) });
